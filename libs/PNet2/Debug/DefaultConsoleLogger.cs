de2f7e4529265d54a7fa74af1c1cd38cbb3c1322
Ignis34Rus <vovan275@gmail.com>
2016-12-23 22:22:05 +0300

    missed files

---
+++ libs/PNet2/Debug/DefaultConsoleLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PNet
+{
+    /// <summary>
+    /// Console recipient for the log
+    /// </summary>
+    public sealed class DefaultConsoleLogger : ILogger
+    {
+        /// <summary>
+        /// Info
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="args"></param>
+        public void Info(string info, params object[] args)
+        {
+            Console.WriteLine(info, args);
+        }
+
+        /// <summary>
+        /// Warning
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="args"></param>
+        public void Warning(string info, params object[] args)
+        {
+            Console.WriteLine(info, args);
+        }
+
+        /// <summary>
+        /// error
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="args"></param>
+        public void Error(string info, params object[] args)
+        {
+            Console.WriteLine(info, args);
+        }
+
+        /// <summary>
+        /// exception
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="exception"></param>
+        /// <param name="args"></param>
+        public void Exception(Exception exception, string info, params object[] args)
+        {
+            Console.WriteLine(string.Format(info, args) + " ex: {0}", exception);
+        }
+    }
+}
---
+++ libs/PNet2/Debug/ILogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PNet
+{
+    /// <summary>
+    /// Interface for logging information
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// informational message
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="args"></param>
+        void Info(string info, params object[] args);
+        /// <summary>
+        /// warning message
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="args"></param>
+        void Warning(string info, params object[] args);
+        /// <summary>
+        /// error message
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="args"></param>
+        void Error(string info, params object[] args);
+
+        /// <summary>
+        /// Exception
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="exception"></param>
+        /// <param name="args"></param>
+        void Exception(Exception exception, string info, params object[] args);
+    }
+}
